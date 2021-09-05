23f859f74f6759a6e10641eeb15a8d2cea4ed801
freen <dbickett@gmail.com>
2021-09-05 12:31:27 -0400

    RosterParser

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 import fs from 'fs';
 import pdf from 'pdf-parse';
+import RosterParser from './lib/RosterParser';
 
 let dataBuffer = fs.readFileSync('roster.pdf');
 
-pdf(dataBuffer).then(function(data) {
+pdf(dataBuffer).then((data) => {
 
-    // number of pages
-    console.log(data.numpages);
-    // number of rendered pages
-    console.log(data.numrender);
-    // PDF info
-    console.log(data.info);
-    // PDF metadata
-    console.log(data.metadata);
-    // PDF.js version
-    // check https://mozilla.github.io/pdf.js/getting_started/
-    console.log(data.version);
-    // PDF text
-    console.log(data.text);
+  // For inspection
+  fs.promises
+    .open('roster.txt', 'w+')
+    .then((fileHandle) => {
+      return fileHandle.write(data.text);
+    });
+
+  const rp = new RosterParser(data.text);
+
+
 
 });
--- notes.md
+++ notes.md
@@ -1,3 +1,5 @@
+# Features
+
 - User location never shared, local determination of proximity
 - Determination of proximity could be:
   - Crow-flies, quick / initial sort: Simple calculation based on coords, or
@@ -6,3 +8,28 @@
   - Directly from DOJ OLAP PDF, parse
   - Fallback to API, which has its own cache
   - Fallback to local cache, if exists, or if its more up to date than API cache
+
+
+# PDF Text Demarcations
+
+## End of one RA, beg of another
+
+Active
+
+## End of one state, beginning of another
+
+Return to the top of the page
+[NEW STATE NAME]
+
+## End of RAs, beginning of ARs
+
+Return to the top of the page
+
+Recognized
+Organization
+Accredited
+Representative
+Accreditation
+Expiration Date
+Representative
+Status
