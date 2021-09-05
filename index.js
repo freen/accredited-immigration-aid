1eb4e1ad2012364caae22050b9f5e74527beba15
freen <dbickett@gmail.com>
2021-09-05 14:29:23 -0400

    pathing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import pdf from 'pdf-parse';
-import RosterParser from './lib/RosterParser';
+import RosterParser from 'RosterParser';
 
 let dataBuffer = fs.readFileSync('roster.pdf');
 
