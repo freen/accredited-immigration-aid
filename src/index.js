ebe34acec24d3321fb8618186d374784ef304c6a
freen <dbickett@gmail.com>
2021-09-11 10:20:16 -0400

    pdf2html

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import fs from 'fs';
-import pdf from 'pdf-parse';
+// import pdf from 'pdf-parse';
 import RosterParser from './lib/RosterParser';
+import pdf2html from 'pdf2html';
 
-let dataBuffer = fs.readFileSync('roster.pdf');
+// let dataBuffer = fs.readFileSync('roster.pdf');
 
-pdf(dataBuffer).then((data) => {
+// pdf(dataBuffer).then((data) => {
 
-  // For inspection
-  fs.promises
-    .open('roster.txt', 'w+')
-    .then((fileHandle) => {
-      return fileHandle.write(data.text);
-    });
+//   // For inspection
+//   fs.promises
+//     .open('roster.txt', 'w+')
+//     .then((fileHandle) => {
+//       return fileHandle.write(data.text);
+//     });
 
-  const rp = new RosterParser(data.text);
+//   const rp = new RosterParser(data.text);
 
+// });
+
+pdf2html.html('roster.pdf', (err, html) => {
+  if (err) {
+    console.error('Conversion error: ' + err)
+  } else {
+    fs.promises
+      .open('roster.html', 'w+')
+      .then((fileHandle) => {
+        return fileHandle.write(html);
+      });
+  }
 });
